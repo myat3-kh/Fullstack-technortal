cfcf4fa6f2d3eed72f47dd22fabf323cc7902fc3
myat3-kh <mmyatsandiaung67@gmail.com>
2026-02-19 23:03:27 +0630

    update project

--- furniture/src/lib/auth-client.ts
+++ furniture/src/lib/auth-client.ts
@@ -1,7 +1,13 @@
 import { createAuthClient } from "better-auth/react";
-export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:8080",
-});
+//import { createAuthClient } from "better-auth/react";
+import { emailOTPClient } from "better-auth/client/plugins";
 
-export const { signIn, signUp, signOut, useSession } = authClient;
+export const { signIn, signUp, signOut, useSession, emailOtp } =
+  createAuthClient({
+    /** The base URL of the server (optional if you're using the same domain) */
+    baseURL: "http://localhost:8080",
+    fetchOptions: {
+      credentials: "include",
+    },
+    plugins: [emailOTPClient()],
+  });
--- furniture/vite.config.ts
+++ furniture/vite.config.ts
@@ -5,6 +5,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // Next.js Backend URL
+        changeOrigin: true,
+      },
+    },
+  },
+
   plugins: [
     react({
       babel: {
