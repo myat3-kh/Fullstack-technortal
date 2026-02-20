12449d0898c4e6f4251d59f5743154693d3e72d2
myat3-kh <mmyatsandiaung67@gmail.com>
2026-02-20 22:31:32 +0630

    update project

--- express-api/src/lib/prisma.ts
+++ express-api/src/lib/prisma.ts
@@ -1,20 +1,45 @@
+import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+import ws from "ws";
+neonConfig.webSocketConstructor = ws;
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+// neonConfig.poolQueryViaFetch = true
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
+// Type definitions
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const connectionString = " ${process.env.DATABASE_URL}";
+
+const adapter = new PrismaNeon({ connectionString });
+const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 export default prisma;
+
+// import { PrismaClient } from "../generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient;
+// };
+
+// const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     adapter,
+//   });
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
